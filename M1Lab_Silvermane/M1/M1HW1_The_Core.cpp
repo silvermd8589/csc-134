70951eaad003ceb1b6176c49944089691f0b04ed
Daniel S <silvermd8589@student.faytechcc.edu>
2026-09-02 15:15:27 +0000

    correct number of qoutes and triva

--- M1Lab_Silvermane/M1/M1HW1_The_Core.cpp
+++ M1Lab_Silvermane/M1/M1HW1_The_Core.cpp
@@ -26,8 +26,16 @@ int main() {
     cout << "Its grossed in total $" << movie_grossm << " million." << endl;
 
     //Print a movie qoute 
-    cout << "Qoute:" << endl << " \t\"Look I had this ship built in three damn months, I didn't think I'd deliberately be sabotaging her! \" " << endl;
-    cout << "Qoute:" << 
+    cout << "Qoute:" << endl << " \t\"Look I had this ship built in three damn months, I didn't think I'd deliberately be sabotaging her!\" " << endl;
+    cout << "Qoute:" << endl << " \t\"You're asking me to navigate a vessel through solid rock, pressurized magma, and unknown densities using a hull made out of Unobtainium.\" " << endl;
+    cout << "Qoute:" << endl << " \t\"We are talking about the end of the world. Not next week, not tomorrow, right now \" " <<endl;
+    cout << "Qoute:" << endl << " \t\" The core has stopped spinning. \" " << endl;
+
+    // Print triva 
+    cout << "triva:" << endl << "\t\"The Unobtainium Riddle: The fictional material used to build the ship, \"Unobtainium,\" is a classic aerospace engineering industry joke for a material that has ideal properties but is impossible to get.\"" << endl;
+    cout << "triva:" << endl << "\t\"Box Office Run: Despite a reported production budget of around $60 million, the film grossed just over $74 million worldwide, making it a box office bomb.\"" << endl;
+
+
 
 
 
