27ac979e1d0488c4b69cccb03a4bfeaa63afe427
Daniel S <silvermd8589@student.faytechcc.edu>
2026-09-14 14:45:48 +0000

    the start of the receipt

---
+++ M1Lab_Silvermane/M2/M2T2_silvermane.cpp
@@ -0,0 +1,39 @@
+//CSC-134
+// SIlvermane
+// M2T2
+//9/14/26
+// Reciepts 
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+//declare variables
+string meal_name;    // ex "Chicken Sandwich"
+double meal_price;   //$
+double tax_rate;     // precent
+double tax_ammount;  // $
+double total;        // $, meal + tax 
+
+
+
+// inputs
+// right now nothing, the pick exactly sandwich 
+meal_name = "Chicken Sandwich";
+meal_price = 5.99;
+tax_rate =   0.08;
+
+//processing 
+
+
+
+//output 
+
+
+
+
+    return 0; //no errors 
+}
