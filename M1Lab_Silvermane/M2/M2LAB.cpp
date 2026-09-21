773afe704a0f91823e5c213d899e9a7426f8bde8
Daniel S <silvermd8589@student.faytechcc.edu>
2026-09-21 15:36:13 +0000

    start

---
+++ M1Lab_Silvermane/M2/M2LAB.cpp
@@ -0,0 +1,28 @@
+//CSC-134
+// SIlvermane
+// M2LAB
+//9/21/26
+// Crates 
+
+
+#include <isostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    //constants for cost and amount charged 
+    const double COST_PER_CUBIC_FOOT = 0.23;
+    const double CHARGE_PER_CUBIC_FOOT = .05;
+
+    //Variables 
+    double length, // the crates lenght 
+    double width, // "" width
+    double height, // "" height 
+    double cost, // the cost to build the crate 
+    double charge, // the customer charge for the crate 
+    double profit, // the profit made 
+
+    //set the desired output formatting for numbers 
+    cout << 
+}
