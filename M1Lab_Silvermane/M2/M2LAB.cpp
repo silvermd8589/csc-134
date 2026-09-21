04733dae508a34815bad8091bbeb3ab432fd873f
Daniel S <silvermd8589@student.faytechcc.edu>
2026-09-21 15:45:44 +0000

    done

--- M1Lab_Silvermane/M2/M2LAB.cpp
+++ M1Lab_Silvermane/M2/M2LAB.cpp
@@ -13,7 +13,7 @@ int main()
 {
     //constants for cost and amount charged 
     const double COST_PER_CUBIC_FOOT = 0.23;
-    const double CHARGE_PER_CUBIC_FOOT = .05;
+    const double CHARGE_PER_CUBIC_FOOT = 0.89;
 
     //Variables 
         double length, // the crate's length
@@ -34,5 +34,18 @@ int main()
     cout << "Height  ";
     cin >> height;
 
-    
+    // calc the crates volume, the cost to build the crate, the customer charge for the crate, and the profit made
+    double volume = length * width * height;
+    cost = volume * COST_PER_CUBIC_FOOT;
+    charge = volume * CHARGE_PER_CUBIC_FOOT;
+    profit = charge - cost; 
+
+    // display the results
+    cout << "\nThe volume of the crate is " << volume << " cubic feet." << endl;
+    cout << "The cost to build the crate is $" << cost << endl;
+    cout << "The customer charge for the crate is $" << charge << endl;
+    cout << "The profit made is $" << profit << endl;
+
+
+    return 0;
 }
