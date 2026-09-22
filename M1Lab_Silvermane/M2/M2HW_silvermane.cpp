951e50231b3696e27c9d0ce7db22753c13e9a30d
Daniel S <silvermd8589@student.faytechcc.edu>
2026-09-22 02:55:24 +0000

    ughh

---
+++ M1Lab_Silvermane/M2/M2HW_silvermane.cpp
@@ -0,0 +1,35 @@
+//CSC-134
+// SIlvermane
+// M2HW
+//9/21/26
+// 
+
+using namespace std;
+#include <iostream>
+#include <iomanip>
+
+
+int main() {
+    //Set up variables
+    string account_type;
+     int account_checking = 1;
+     int account_savings = 2;
+     int account_401k = 3;
+
+
+
+
+    cout << "Question 1. " << endl; 
+    cout << "Welcome to Silver Credit " << endl;
+    cout << "Insert card" << endl;
+    cout << "BEEB BOP DAIL UP NOISE" << endl;
+    cout << "which acount would you like" << endl;
+    cout << "1. Checkings, ";
+    cout << "2. Savings, ";
+    cout << "2. 401k. " << endl;
+    cin >> account_type; 
+    
+
+
+    return 0; //no errors
+}
