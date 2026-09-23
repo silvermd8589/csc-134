d8b0d2eb8fd5bdee88a27b3c7dc3ec68e6b18c42
Daniel S <silvermd8589@student.faytechcc.edu>
2026-09-23 14:39:41 +0000

    starting out

---
+++ M1Lab_Silvermane/M3/M3T2_Silvermane.cpp
@@ -0,0 +1,20 @@
+//CSC-134
+// SIlvermane
+// M3T
+//9/2/26
+// Start writing a simple "Craps" casino game 
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+
+
+    cout << rand() << " " << rand() << endl;
+    
+
+
+    return 0; //no errors 
+}
