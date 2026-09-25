679cedead76dce81c73a8113416cb6218a304e80
Daniel S <silvermd8589@student.faytechcc.edu>
2026-09-25 13:49:50 +0000

    something aint right

--- M1Lab_Silvermane/M2/M2HW_silvermane_q3.cpp
+++ M1Lab_Silvermane/M2/M2HW_silvermane_q3.cpp
@@ -17,7 +17,8 @@ int main() {
     const int slices_per_pie = 8;
     int num_people, total_slices;
     int num_pieces;
-    int total_pizzas;
+    double total_pizzas;
+    int leftover_slices;
 
     cout << "The phone on the other line rang." << endl;
     cout << "Cindy answered with a hello." << endl;
@@ -25,8 +26,10 @@ int main() {
     cin >> num_people;
     total_slices = num_people * slice_per_person;
     cout << "we need " << total_slices << " slices of pizza." << endl;
-    total_pizzas = ceil(slice_per_person / slices_per_pie);
+    total_pizzas = ceil(total_slices / slices_per_pie);
     cout << "we need " << total_pizzas << " pies of pizza." << endl;
+    leftover_slices = (total_slices / total_slices + 1);
+    cout << "We have " << leftover_slices << " slices leftover";
 
 
 
